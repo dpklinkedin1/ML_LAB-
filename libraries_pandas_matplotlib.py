0cb79e8e37ae4366ea4524276c808dd9427f40a2
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-03-26 07:10:30 +0530

    Update libraries_pandas_matplotlib.py

--- libraries_pandas_matplotlib.py
+++ libraries_pandas_matplotlib.py
@@ -1,1 +1,27 @@
+import pandas as pd
 
+s = pd.Series([10,20,30])
+print(s)
+data = {
+    "Name": ["A","B","C"],
+    "Marks": [90,80,85]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+df["Marks"] = df["Marks"].fillna(df["Marks"].mean())
+df.head()
+df.tail()
+df.info()
+df.describe()
+
+df["Marks"]
+df.iloc[0]
+df.loc[0]
+
+df[df["Marks"] > 80]
+
+df["Grade"] = ["A","B","A"]
+df.dropna()
+df.fillna(0)
