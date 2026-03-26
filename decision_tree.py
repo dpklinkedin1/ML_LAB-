0342886b0f032f3fc97194f519f53576ec4439ec
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-03-26 09:05:24 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,1 +1,31 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
 
+# Dataset
+data = {
+    "Area": [1000,1500,2000,2500,3000],
+    "Bedrooms": [2,3,3,4,4],
+    "Age": [10,5,8,3,1],
+    "Price": [200000,300000,350000,400000,500000]
+}
+
+df = pd.DataFrame(data)
+
+# Features & Target
+X = df[["Area","Bedrooms","Age"]]
+y = df["Price"]
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Model
+model = DecisionTreeRegressor()
+
+# Train
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+print("Predictions:", y_pred)
