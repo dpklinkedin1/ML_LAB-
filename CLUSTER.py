02fc6b5a9ee17de65f18e7c906a942b231aa0f3c
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-04-08 12:14:39 +0530

    Update CLUSTER.py

--- CLUSTER.py
+++ CLUSTER.py
@@ -1,1 +1,27 @@
+import numpy as np
+from sklearn.cluster import KMeans
 
+# Data (2D points)
+X = np.array([
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [8, 7],
+    [9, 8],
+    [10, 9]
+])
+
+# Create model (K = 2 clusters)
+model = KMeans(n_clusters=2)
+
+# Train model
+model.fit(X)
+
+# Get cluster labels
+labels = model.labels_
+
+# Get centroids
+centroids = model.cluster_centers_
+
+print("Cluster Labels =", labels)
+print("Centroids =", centroids)
