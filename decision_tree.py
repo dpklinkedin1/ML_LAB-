8ef2f3193123b1109870f24c6d28c1ee7592d567
dpklinkedin1 <dpklinkedin1@gmail.com>
2026-04-08 12:00:17 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,31 +1,18 @@
-import pandas as pd
+import numpy as np
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split
 
-# Dataset
-data = {
-    "Area": [1000,1500,2000,2500,3000],
-    "Bedrooms": [2,3,3,4,4],
-    "Age": [10,5,8,3,1],
-    "Price": [200000,300000,350000,400000,500000]
-}
+X = np.array([
+    [1000,2,10],
+    [1500,3,5],
+    [1800,4,2],
+    [1200,2,8],
+    [2000,5,1]
+])
 
-df = pd.DataFrame(data)
-
-# Features & Target
-X = df[["Area","Bedrooms","Age"]]
-y = df["Price"]
-
-# Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Model
+y = np.array([200000,300000,400000,250000,500000])
 model = DecisionTreeRegressor()
+model.fit(X, y)
+new_house = np.array([[1600,3,4]])
+price = model.predict(new_house)
 
-# Train
-model.fit(X_train, y_train)
-
-# Predict
-y_pred = model.predict(X_test)
-
-print("Predictions:", y_pred)
+print("Predicted Price =", price[0])
